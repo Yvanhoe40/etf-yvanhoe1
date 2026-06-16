b42d6c39ed61ed39a4c431098a3dab5cead71529
Yvanhoe40 <yvanhoe40@gmail.com>
2026-06-16 17:04:32 +0200

    Test Twelve Data search quote and time series

--- src/app/api/collect-market-data/route.ts
+++ src/app/api/collect-market-data/route.ts
@@ -8,18 +8,43 @@ export async function GET() {
     );
   }
 
-  const quoteUrl = new URL("https://api.twelvedata.com/quote");
+  const symbol = "NUCL";
+  const exchange = "EURONEXT";
 
-  quoteUrl.searchParams.set("symbol", "NUCL");
-  quoteUrl.searchParams.set("exchange", "EURONEXT");
+  const searchUrl = new URL("https://api.twelvedata.com/symbol_search");
+  searchUrl.searchParams.set("symbol", symbol);
+  searchUrl.searchParams.set("apikey", apiKey);
+
+  const quoteUrl = new URL("https://api.twelvedata.com/quote");
+  quoteUrl.searchParams.set("symbol", symbol);
+  quoteUrl.searchParams.set("exchange", exchange);
   quoteUrl.searchParams.set("apikey", apiKey);
 
-  const response = await fetch(quoteUrl.toString());
-  const data = await response.json();
+  const timeSeriesUrl = new URL("https://api.twelvedata.com/time_series");
+  timeSeriesUrl.searchParams.set("symbol", symbol);
+  timeSeriesUrl.searchParams.set("exchange", exchange);
+  timeSeriesUrl.searchParams.set("interval", "1day");
+  timeSeriesUrl.searchParams.set("outputsize", "5");
+  timeSeriesUrl.searchParams.set("apikey", apiKey);
+
+  const [searchResponse, quoteResponse, timeSeriesResponse] =
+    await Promise.all([
+      fetch(searchUrl.toString()),
+      fetch(quoteUrl.toString()),
+      fetch(timeSeriesUrl.toString()),
+    ]);
+
+  const [searchData, quoteData, timeSeriesData] = await Promise.all([
+    searchResponse.json(),
+    quoteResponse.json(),
+    timeSeriesResponse.json(),
+  ]);
 
   return Response.json({
-    testedSymbol: "NUCL",
-    testedExchange: "EURONEXT",
-    twelveDataResponse: data,
+    testedSymbol: symbol,
+    testedExchange: exchange,
+    symbolSearch: searchData,
+    quote: quoteData,
+    timeSeries: timeSeriesData,
   });
 }
